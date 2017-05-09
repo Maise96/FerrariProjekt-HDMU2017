8ee7b86997c7dfb9298df94d0beccfd038a696f0
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-05-09 09:28:55 +0200

    ffl og ffl_devtools er importet og der er eksempel på at bruge dem i Main

--- src/launch/Main.java
+++ src/launch/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import presentation.PrimaryStage;
 
+import com.ferrari.finances.dk.bank.*;
+
 public class Main extends Application {
 
 	public static void main(String[] args) {
